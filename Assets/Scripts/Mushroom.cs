064761ef03261f240a05f4c46ed86ef2c3120def
raihanmaziz <raihan441@gmail.com>
2022-08-29 13:16:38 +0800

    Adding object pooling

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -6,14 +6,34 @@ public class Mushroom : MonoBehaviour
 {
     public float upForce = 1f;
     public float sideForce = .1f;
-    void Start()
+
+    private float _despawnTimer = 0f;
+    private float _despawnDelay = 3f;
+
+    private void OnEnable()
     {
+        _despawnTimer = 0f;
         float xForce = Random.Range(-sideForce, sideForce);
-        float yForce = Random.Range(upForce/2f, upForce);
+        float yForce = Random.Range(upForce / 2f, upForce);
         float zForce = Random.Range(-sideForce, sideForce);
 
-        Vector3 force = new Vector3 (xForce, yForce, zForce);
+        Vector3 force = new Vector3(xForce, yForce, zForce);
 
         GetComponent<Rigidbody>().velocity = force;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        _despawnTimer += Time.deltaTime;
+        if ( _despawnTimer > _despawnDelay)
+        {
+            gameObject.SetActive(false);
+            _despawnTimer = 0;
+        }
     }
 }
--- Assets/Scripts/MushroomSpawner.cs
+++ Assets/Scripts/MushroomSpawner.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 public class MushroomSpawner : MonoBehaviour
 {
     public GameObject objPrefab;
+
+    private List<GameObject> _mushroomPool = new List<GameObject>();
     
     void FixedUpdate(){
-        Instantiate(objPrefab, transform.position, Quaternion.identity);
+        InstantiateMushroom(objPrefab, _mushroomPool);
+    }
+
+    void InstantiateMushroom(GameObject prefab, List<GameObject> pool)
+    {
+        GameObject mushroom = pool.Find(i => !i.gameObject.activeSelf);
+        if (mushroom == null)
+        {
+            mushroom = Instantiate(prefab, transform.position, Quaternion.identity);
+            pool.Add(mushroom);
+        }
+        mushroom.transform.position = transform.position;
+        mushroom.gameObject.SetActive(true);
     }
 }
